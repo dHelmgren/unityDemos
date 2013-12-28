033679335d0ff50451eb839e7d8c34c7fd527539
Devin Helmgren <devincollege@gmail.com>
2013-12-28 13:51:30 -0800

    Ball Rolling Project, ball rolls

---
+++ BallRollin/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	void Update(){
+
+	}
+	public float speed;//appears in the editor
+
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rigidbody.AddForce (movement * speed * Time.deltaTime);//time.deltaTime
+
+	}
+}
